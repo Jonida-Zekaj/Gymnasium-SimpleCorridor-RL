f666dcf3cd7bf95341d42a4df1bc82fc425cf897
Jonida-Zekaj <jonidazekaj04@gmail.com>
2026-03-29 15:46:29 +0200

    Initial commit - simple plotting via matplotlib.

---
+++ visualization/plot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt 
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+
+def plot_rewards(log_folder):
+  x, y=ts2xy(load_results(log_folder), 'timesteps')
+  plt.figure(figsize=(10,5))
+  plt.plot(x,y)
+  plt.xlabel("Timesteps")
+  plt.ylabel("Reward per Episode")
+  plt.title("Reward versus Timesteps")
+  plt.grid()
+  plt.legend()
+  plt.show
